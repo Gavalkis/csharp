d59eb68a3947f2cc556259a940381f137eaeed6b
Gavalkis <91756391+Gavalkis@users.noreply.github.com>
2025-05-26 20:42:31 +0300

    Update RealisationHistoryPage.xaml.cs

--- DemoTrain/Pages/RealisationHistoryPage.xaml.cs
+++ DemoTrain/Pages/RealisationHistoryPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,18 +20,11 @@ namespace masterAndFloorApp
     /// </summary>
     public partial class RealisationHistoryPage : Page
     {
-        private List<Partner> _partners;
-        private List<PartnerProduct> _partnerProducts;
         public RealisationHistoryPage()
         {
             InitializeComponent();
+            CBPartners.ItemsSource = masterAndFloorEntities1.GetContext().Partners.ToList();
             DGridPartnerProducts.ItemsSource = masterAndFloorEntities1.GetContext().PartnerProducts.ToList();
-
-            _partners = masterAndFloorEntities1.GetContext().Partners.ToList();
-            CBPartners.ItemsSource = _partners;
-
-            _partnerProducts = masterAndFloorEntities1.GetContext().PartnerProducts.Include("Product").Include("Partner").ToList();
-            DGridPartnerProducts.ItemsSource = _partnerProducts;
         }
 
         private void FilterHistory()
@@ -40,41 +33,32 @@ namespace masterAndFloorApp
             DateTime? startDate = DPStartDate.SelectedDate;
             DateTime? endDate = DPEndDate.SelectedDate;
 
-            var filteredProducts = masterAndFloorEntities1.GetContext().PartnerProducts.Include("Product").Include("Partner").AsQueryable();
+            var filteredProducts = masterAndFloorEntities1.GetContext().PartnerProducts.AsQueryable();
 
             if (selectedPartner != null)
             {
-                filteredProducts = filteredProducts.Where(pp => pp.PartnerID == selectedPartner.PartnerID);
+                filteredProducts = filteredProducts.Where(pp=>pp.PartnerID == selectedPartner.PartnerID);
             }
-
-            // Фильтрация по дате
             if (startDate.HasValue)
             {
                 filteredProducts = filteredProducts.Where(pp => pp.Date >= startDate.Value);
             }
             if (endDate.HasValue)
             {
-                filteredProducts = filteredProducts.Where(pp => pp.Date <= endDate.Value);
+                filteredProducts = filteredProducts.Where(pp => pp.Date == endDate.Value);
             }
-
-            if(IsFilter.IsChecked == false)
+            if (IsFilter.IsChecked == false)
             {
-                filteredProducts = masterAndFloorEntities1.GetContext().PartnerProducts.Include("Product").Include("Partner").AsQueryable();
+                filteredProducts = masterAndFloorEntities1.GetContext().PartnerProducts.AsQueryable();
             }
+            DGridPartnerProducts.ItemsSource = filteredProducts.ToList();
 
+        }
+        private void IsFilter_Click(object sender, RoutedEventArgs e)
+        {
+            var filteredProducts = masterAndFloorEntities1.GetContext().PartnerProducts.Include("Product").Include("Partner").AsQueryable();
             DGridPartnerProducts.ItemsSource = filteredProducts.ToList();
         }
-
-        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            if (Visibility == Visibility.Visible)
-            {
-                masterAndFloorEntities1.GetContext().ChangeTracker.Entries().ToList().ForEach(a => a.Reload());
-                _partnerProducts = masterAndFloorEntities1.GetContext().PartnerProducts.Include("Product").Include("Partner").ToList();
-                DGridPartnerProducts.ItemsSource = _partnerProducts;
-            }
-        }
-
         private void CBPartners_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             FilterHistory();
@@ -85,19 +69,23 @@ namespace masterAndFloorApp
             FilterHistory();
         }
 
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                masterAndFloorEntities1.GetContext().ChangeTracker.Entries().ToList().ForEach(a => a.Reload());
+                DGridPartnerProducts.ItemsSource = masterAndFloorEntities1.GetContext().PartnerProducts.Include("Product").Include("Partner").ToList();
+            }
+        }
+
         private void DeleteBtn_Click(object sender, RoutedEventArgs e)
         {
-            //Список на удаление
             var PartnerProductsForRemove = DGridPartnerProducts.SelectedItems.Cast<PartnerProduct>().ToList();
             if (PartnerProductsForRemove.Count <= 0)
-            {
                 return;
-            }
-            //Окно выбора да/нет
-            if (MessageBox.Show($"Вы уверены, что хотите удалить следущие {PartnerProductsForRemove.Count()} элементов?", "Внимание",
+            if(MessageBox.Show($"Вы уверены, что хотите удалить следующие {PartnerProductsForRemove.Count} элементов?", "Внимание", 
                 MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
             {
-                //Если да, то выполняется попытка удаления данных
                 try
                 {
                     masterAndFloorEntities1.GetContext().PartnerProducts.RemoveRange(PartnerProductsForRemove);
@@ -105,14 +93,11 @@ namespace masterAndFloorApp
                     MessageBox.Show("Данные успешно удалены!");
                     DGridPartnerProducts.ItemsSource = masterAndFloorEntities1.GetContext().PartnerProducts.ToList();
                 }
-                //Иначе выводит ошибку
                 catch (Exception ex)
                 {
                     MessageBox.Show(ex.Message.ToString());
                 }
-
             }
-
 
         }
 
@@ -126,5 +111,7 @@ namespace masterAndFloorApp
         {
             NavigationClass.MainFrame.Navigate(new RH_AddEditPage(null));
         }
+
+        
     }
 }
