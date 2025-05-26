5021d5f237452d28ad6b22c11dcd2fc9ac1d90bf
Gavalkis <91756391+Gavalkis@users.noreply.github.com>
2025-05-26 18:30:52 +0300

    Add files via upload

---
+++ DemoTrain/Pages/AddEditPage.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace masterAndFloorApp
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+
+    public partial class AddEditPage : Page
+    {
+        private Partner _currentPartner = new Partner();
+        public Partner Partner
+        {
+            get => _currentPartner;
+            set => _currentPartner = value;
+        }
+
+        public List<PartnerType> PartnerTypes { get; set; } // Изменено на свойство
+
+        public AddEditPage(Partner selectedPartner)
+        {
+            InitializeComponent();
+
+            // Загрузка типов партнеров
+            PartnerTypes = masterAndFloorEntities1.GetContext().PartnerTypes.ToList();
+
+            if (selectedPartner != null)
+            {
+                _currentPartner = selectedPartner;
+            }
+
+            DataContext = this; // Устанавливаем DataContext для всей страницы
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder err = new StringBuilder();
+            if (CBPartnerTypes.SelectedValue == null)
+            {
+                err.AppendLine("Выберете тип партнера");
+            }
+            if (string.IsNullOrWhiteSpace(_currentPartner.Title))
+                err.AppendLine("Укажите наименование");
+            if (string.IsNullOrWhiteSpace(_currentPartner.Director))
+                err.AppendLine("Укажите ФИО Директора");
+            if (string.IsNullOrWhiteSpace(_currentPartner.Email))
+                err.AppendLine("Укажите Эл. почту");
+            if (string.IsNullOrWhiteSpace(_currentPartner.Phone))
+                err.AppendLine("Укажите Телефон");
+            if (string.IsNullOrWhiteSpace(_currentPartner.LegalAddress))
+                err.AppendLine("Укажите Юр адрес");
+            if (string.IsNullOrWhiteSpace(_currentPartner.INN))
+                err.AppendLine("Укажите ИНН");
+            if (string.IsNullOrWhiteSpace(_currentPartner.Rating.ToString()))
+                err.AppendLine("Укажите Рейтинг");
+            if (_currentPartner.Rating % 10 != 0)
+                err.AppendLine("Укажите целое значение рейтинга");
+            if (_currentPartner.Rating < 0)
+                err.AppendLine("Укажите не отрицательное  значение рейтинга");
+
+            if (err.Length > 0)
+            {
+                MessageBox.Show(err.ToString());
+                return;
+            }
+
+            if (_currentPartner.PartnerID == 0)
+                masterAndFloorEntities1.GetContext().Partners.Add(_currentPartner);
+
+            try
+            {
+                masterAndFloorEntities1.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Окно выбора да/нет
+            if (MessageBox.Show($"Вы уверены, что хотите удалить данного партнера: {_currentPartner.Title}?", "Внимание",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                //Если да, то выполняется попытка удаления данных
+                try
+                {
+                    masterAndFloorEntities1.GetContext().Partners.Remove(_currentPartner);
+                    masterAndFloorEntities1.GetContext().SaveChanges();
+                    MessageBox.Show("Данные успешно удалены!");
+                    NavigationClass.MainFrame.Navigate(new PartnersPage());
+                }
+                //Иначе выводит ошибку
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
+            }
+        }
+    }
+
+}
---
+++ DemoTrain/Pages/PartnersPage.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace masterAndFloorApp
+{
+    /// <summary>
+    /// Логика взаимодействия для PartnersPage.xaml
+    /// </summary>
+    public partial class PartnersPage : Page
+    {
+        
+
+        public PartnersPage()
+        {
+            InitializeComponent();
+            PartnersItemsControl.ItemsSource = masterAndFloorEntities1.GetContext().Partners.ToList();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationClass.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationClass.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Partner));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(Visibility == Visibility.Visible)
+            {
+                
+                CalculateDiscounts();
+                masterAndFloorEntities1.GetContext().ChangeTracker.Entries().ToList().ForEach(a => a.Reload());
+                PartnersItemsControl.ItemsSource = masterAndFloorEntities1.GetContext().Partners.ToList();
+            }   
+        }
+
+        public void CalculateDiscounts()
+        {
+            //Явно получаем данные о партнерах
+            var partners = masterAndFloorEntities1.GetContext().Partners.Include("PartnerProducts").ToList();
+
+            foreach (var partner in partners)
+            {
+                //Складываем количество проданной продукции определенного партнера
+                long totalQuantity = partner.PartnerProducts.Sum(pp => pp.Quantity);
+                // Устанавливаем значение Discount
+                if (totalQuantity < 10000)
+                    partner.Discount = 0f; // 0% скидка
+                else if (totalQuantity < 50000)
+                    partner.Discount = 5f; // 5% скидка
+                else if (totalQuantity < 300000)
+                    partner.Discount = 10f; // 10% скидка
+                else
+                    partner.Discount = 15f; // 15% скидка
+            }
+            try
+            {
+                masterAndFloorEntities1.GetContext().SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private float CalculateDiscount(long totalQuantity)
+        {
+            if (totalQuantity < 10000)
+                return 0f; // 0% скидка
+            else if (totalQuantity < 50000)
+                return 5f; // 5% скидка
+            else if (totalQuantity < 300000)
+                return 10f; // 10% скидка
+            else
+                return 15f; // 15% скидка
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var border = sender as Border;
+            if (border != null)
+            {
+                // Получаем данные о партнере из DataContext
+                var partner = border.DataContext as Partner; // Замените Partner на ваш класс модели
+
+                // Переход на страницу AddEdit с передачей данных о партнере
+                var addEditPage = new AddEditPage(partner); // Предполагается, что у вас есть конструктор, принимающий партнера
+                NavigationService.Navigate(addEditPage);
+            }
+        }
+
+    }
+}
