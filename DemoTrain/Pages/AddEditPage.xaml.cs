874af2fbd57467718a03170c06aeba141e79bc31
Gavalkis <91756391+Gavalkis@users.noreply.github.com>
2025-05-26 19:12:36 +0300

    Update AddEditPage.xaml.cs

--- DemoTrain/Pages/AddEditPage.xaml.cs
+++ DemoTrain/Pages/AddEditPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -66,7 +66,7 @@ namespace masterAndFloorApp
                 err.AppendLine("Укажите ИНН");
             if (string.IsNullOrWhiteSpace(_currentPartner.Rating.ToString()))
                 err.AppendLine("Укажите Рейтинг");
-            if (_currentPartner.Rating % 10 != 0)
+            if (_currentPartner.Rating / 10 != 0)
                 err.AppendLine("Укажите целое значение рейтинга");
             if (_currentPartner.Rating < 0)
                 err.AppendLine("Укажите не отрицательное  значение рейтинга");
